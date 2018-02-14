112420c0af68be981019e612bc59abb5c11935d2
alumezi <arbnorlumezi@gmail.com>
2018-02-14 17:58:38 +0100

    ui, controlled components, and build

--- src/App.js
+++ src/App.js
@@ -21,16 +21,23 @@ class App extends Component {
                 L400:[],
         L400expanded:[],
                 L500:[],
-        L500expanded:[]
+        L500expanded:[],
+        Subject    : '',
+        Template   : '',
+        Scale      : '',
+        Assingee   : '',
+        Participants:'',
+        Type:        ''
                 };
 
       this.handleUserInput = this.handleUserInput.bind(this);
+      this.handleClick = this.handleClick.bind(this);
   }
 
   handleUserInput(e){
-    const target = e.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name
+    
+    const value = e.target.value;
+    const name = e.target.name
 
 
     this.setState({
@@ -38,16 +45,24 @@ class App extends Component {
     })
   }
 
+    handleClick(e) {
+      const name = e.target.name;
+      const value = e.target.value;
+    this.setState(({
+      [name]: value
+    }));
+  }
+
 
 
   render() {
 
     return (
-      
-      <div className="all">
+      <div className="full ui form">
+      <div className="all ui raised segment">
 
-        <div className="App">
-              <CheckboxTree
+        <div className="App ">
+              <CheckboxTree 
                 nodes={nodes}
                 checked={this.state.L100}
                 expanded={this.state.L100expanded}
@@ -97,6 +112,71 @@ class App extends Component {
                 onExpand={L500expanded => this.setState({ L500expanded })}
             />
         </div>
+      </div>
+
+  <div className="left ui raised segments">
+
+          <div className="ui buttons segment">
+            <button type="radio" className="ui button">Procedure</button>
+            <button type="radio" className="ui button">Tasks</button>
+            <button type="radio" className="ui button">Dropdown</button>
+            <button type="radio" className="ui button">TIME ENTRIES</button>
+        </div>
+
+        <div className="ui segment">
+            <select className="ui dropdown" onChange={this.handleUserInput} value={this.state.Template} name="Template">
+              <option>Select Template</option>
+              <option value="firstTemplate">First Template</option>
+            </select>
+            <a >Create Manually</a>
+            <a >Narratives <i className="settings"></i></a>
+          </div>
+
+        <div className="ui segment">
+            Subject
+            <input onChange={this.handleUserInput} value={this.state.Subject} name="Subject"/>
+            Scale
+            <select className="ui dropdown" onChange={this.handleUserInput} value={this.state.Scale} name="Scale" >
+              <option value="small">small</option>
+              <option value="medium">medium</option>
+            </select>
+        </div>
+
+        <div className="ui segment">
+            Assingee
+
+            <select className="ui dropdown" onChange={this.handleUserInput} value={this.state.Assingee} name="Assingee">
+              <option>Select Assingee </option>
+              <option value="medium">medium</option>
+            </select>
+
+            Task Code
+            <select className="ui dropdown" onChange={this.handleUserInput} value={this.state.TaskCode} name="TaskCode">
+              <option>Select Code</option>
+              <option value="medium">medium</option>
+            </select>
+        </div>
+
+        <div className="ui buttons segment" >Type 
+          <button  className="ui button"                onClick={this.handleClick} name="Type" value="NonCritical">Non Critical</button>
+          <button type="checkbox" className="ui button" onClick={this.handleClick} name="Type" value="CriticalDeadLine">Critical Deadline</button>
+          <button type="checkbox" className="ui button" onClick={this.handleClick} name="Type" value="CriticalEvent">Critical Event</button>
+        </div>
+
+        External Participants
+
+        <div className="ui segment" >
+          <textarea cols="50" rows="1" onChange={this.handleUserInput} value={this.state.Participants} name="Participants"/><br/>
+          Each participant seperated by a semicolon
+        </div>
+
+
+
+
+
+
+
+  </div>
 
 
 
